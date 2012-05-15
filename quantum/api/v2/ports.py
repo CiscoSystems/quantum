053998b83d34e23bd43a60d094eb4ddf4fe318f5
Matt Dietz <matt.dietz@rackspace.com>
2012-05-15 21:45:19 +0000

    Post-yanked

--- quantum/api/v2/ports.py
+++ quantum/api/v2/ports.py
@@ -17,8 +17,10 @@ import json
 import logging
 
 from quantum.api import faults
+from quantum.api import ports
 from quantum import api_common
 from quantum import wsgi
+from quantum.api.v2 import base
 from quantum.api.v2 import views
 
 LOG = logging.getLogger(__name__)
@@ -61,7 +63,7 @@ def create_resource(plugin, conf):
                          serializer)
 
 
-class ControllerV20(quantum.api.ports.Controller):
+class ControllerV20(ports.Controller, base.Controller):
     _serialization_metadata = {
         "attributes": {
             "port": ["id", "network_id", "mac", "device_id", "tenant_id"]
@@ -70,31 +72,3 @@ class ControllerV20(quantum.api.ports.Controller):
             "ports": "port",
         },
     }
-
-    def _items(self, request):
-        filter_opts = {}
-        filter_opts.update(request.GET)
-        port_list = self._plugin.get_all_ports(filter_opts=filter_opts)
-        return dict(ports=[views.port(port) for port in port_list])
-
-    def _item(self, request, id):
-        port = self._plugin.get_port_details(id)
-        return views.port(port)
-
-    def index(self, req):
-        context = req.environ['quantum.context']
-        self._plugin.get_port_details(tenant_id=context.tenant_id,
-                                      network_id=network_id,
-
-    def show(self, request, id):
-        context = req.environ['quantum.context']
-        return self._item(request, id)
-
-    def create(self, request, body):
-        port_data = api_common._req
-
-    def update(self, request, id, body):
-        pass
-
-    def delete(self, request, id):
-        pass
