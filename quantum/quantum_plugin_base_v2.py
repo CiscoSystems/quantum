3a5ff1f9f9d9eb735671e6bc9de509f04559baae
Jason Kölker <jason@koelker.net>
2012-05-22 15:33:17 -0500

    pep8 fixes
    Change-Id: Ia73f3795b1feadbb017152cfaa06b392fa52ddca

--- quantum/quantum_plugin_base_v2.py
+++ quantum/quantum_plugin_base_v2.py
@@ -22,7 +22,6 @@ QuantumPluginBase provides the definition of minimum set of
 methods that needs to be implemented by a v2 Quantum Plug-in.
 """
 
-import inspect
 from abc import ABCMeta, abstractmethod
 
 
@@ -36,30 +35,30 @@ class QuantumPluginBaseV2(object):
         Create a subnet, which represents a range of IP addresses
         that can be allocated to devices
         : param subnet_data: data describing the prefix
-            {
-              "network_id": UUID of the network to which this subnet
-                            is bound.
-              "ip_version": integer indicating IP protocol version.
-                            example: 4
-              "prefix": string indicating IP prefix indicating addresses
-                        that can be allocated for devices on this subnet.
-                        example: "10.0.0.0/24"
-              "gateway_ip": string indicating the default gateway
-                            for devices on this subnet. example: "10.0.0.1"
-              "dns_nameservers": list of strings stricting indication the
-                                 DNS name servers for devices on this
-                                 subnet.  example: [ "8.8.8.8", "8.8.4.4" ]
-              "excluded_ranges" : list of dicts indicating pairs of IPs that
-                                  should not be allocated from the prefix.
-                                  example: [ { "start" : "10.0.0.2",
-                                               "end" : "10.0.0.5" } ]
-              "additional_routes": list of dicts indicating routes beyond
-                                   the default gateway and local prefix route
-                                   that should be injected into the device.
-                                   example: [ { "destination": "192.168.0.0/16",
-                                                "nexthop": "10.0.0.5" } ]
-              "tags": list of metadata key-value pairs
-            }
+          {
+            "network_id": UUID of the network to which this subnet
+                          is bound.
+            "ip_version": integer indicating IP protocol version.
+                          example: 4
+            "prefix": string indicating IP prefix indicating addresses
+                      that can be allocated for devices on this subnet.
+                      example: "10.0.0.0/24"
+            "gateway_ip": string indicating the default gateway
+                          for devices on this subnet. example: "10.0.0.1"
+            "dns_nameservers": list of strings stricting indication the
+                               DNS name servers for devices on this
+                               subnet.  example: [ "8.8.8.8", "8.8.4.4" ]
+            "excluded_ranges" : list of dicts indicating pairs of IPs that
+                                should not be allocated from the prefix.
+                                example: [ { "start" : "10.0.0.2",
+                                             "end" : "10.0.0.5" } ]
+            "additional_routes": list of dicts indicating routes beyond
+                                 the default gateway and local prefix route
+                                 that should be injected into the device.
+                                 example: [{"destination": "192.168.0.0/16",
+                                              "nexthop": "10.0.0.5" } ]
+            "tags": list of metadata key-value pairs
+          }
         """
         pass
 
@@ -80,16 +79,16 @@ class QuantumPluginBaseV2(object):
         Creates a new Virtual Network, assigns a name and associates
 
         :param net_data:
-                    {
-                     'name': a human-readable name associated
-                             with network referenced by net-id
-                             example: "net-1"
-                     'admin-state-up': indicates whether this network should
-                                       be operational.
-                     'subnets': list of subnet uuids associated with this
-                                network.
-                     'tags': list of metadata key-value pairs
-                    }
+          {
+           'name': a human-readable name associated
+                   with network referenced by net-id
+                   example: "net-1"
+           'admin-state-up': indicates whether this network should
+                             be operational.
+           'subnets': list of subnet uuids associated with this
+                      network.
+           'tags': list of metadata key-value pairs
+          }
         :raises:
         """
         pass
@@ -97,7 +96,6 @@ class QuantumPluginBaseV2(object):
     @abstractmethod
     def update_network(self, auth_context, net_data, **kwargs):
         pass
-
 
     @abstractmethod
     def delete_network(self, auth_context, net_id):
@@ -120,27 +118,31 @@ class QuantumPluginBaseV2(object):
         the subnet.
 
         :param port_data:
-            { "network_id" : UUID of network that this port is attached to.
-              "admin-state-up" : boolean indicating whether this port should be
-                                 operational.
-              "mac_address" : (optional) mac address used on this port.  If no
-                              value is specified, the plugin will generate a MAC
-                              address based on internal configuration.
-              "fixed_ips" : (optional) list of dictionaries describing the fixed
-                            IPs to be allocated for use by the device on this port.
-                            If not specified, the plugin will attempt to find a
-                            v4 and v6 subnet associated with the network and allocate
-                            an IP for that subnet.
-                            Note: "address" is optional, in which case an address
-                                  from the specified subnet is selected.
-                            example: [ { "subnet" : "<uuid>", "address" : "10.0.0.9" } ]
-              "routes" : (optional) list of routes to be injected into this device.
-                         If not specified, the port will get a route for its local subnet,
-                         a route for the default gateway, and each of the routes in the
-                         'additional_routes' field of the subnet.
-                         example: [ { "destination" : "192.168.0.0/16",
-                                      "nexthop" : "10.0.0.5" } ]
-            }
+          {"network_id" : UUID of network that this port is attached to.
+           "admin-state-up" : boolean indicating whether this port should be
+                              operational.
+           "mac_address" : (optional) mac address used on this port.  If no
+                           value is specified, the plugin will generate a
+                           MAC address based on internal configuration.
+           "fixed_ips" : (optional) list of dictionaries describing the
+                         fixed IPs to be allocated for use by the device on
+                         this port. If not specified, the plugin will
+                         attempt to find a v4 and v6 subnet associated
+                         with the network and allocate an IP for that
+                         subnet.
+                         Note: "address" is optional, in which case an
+                               address from the specified subnet is
+                               selected.
+                         example: [{"subnet": "<uuid>",
+                                    "address": "10.0.0.9"}]
+           "routes" : (optional) list of routes to be injected into this
+                      device. If not specified, the port will get a
+                      route for its local subnet, a route for the default
+                      gateway, and each of the routes in the
+                      'additional_routes' field of the subnet.
+                      example: [ { "destination" : "192.168.0.0/16",
+                                   "nexthop" : "10.0.0.5" } ]
+          }
         :raises: exception.NetworkNotFound
         :raises: exception.RequestedFixedIPNotAvailable
         :raises: exception.FixedIPNotAvailable
