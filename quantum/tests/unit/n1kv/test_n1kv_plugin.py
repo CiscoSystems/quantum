df7cc3102475ff2d7b9e7343eff5a66c7776de42
Juergen Brendel <jbrendel@cisco.com>
2013-01-18 15:12:57 +1300

    Added some basic unit tests

---
+++ quantum/tests/unit/n1kv/test_n1kv_plugin.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2012 OpenStack, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from quantum.tests.unit import test_db_plugin as test_plugin
+
+
+class N1kvPluginTestCase(test_plugin.QuantumDbPluginV2TestCase):
+
+    _plugin_name = ('quantum.plugins.cisco.n1kv.'
+                    'n1k_quantum_plugin.N1KQuantumPluginV2')
+
+    def setUp(self):
+        super(N1kvPluginTestCase, self).setUp(self._plugin_name)
+
+
+class TestN1kvBasicGet(test_plugin.TestBasicGet,
+                       N1kvPluginTestCase):
+    def setUp(self):
+        super(N1kvPluginTestCase, self).setUp(self._plugin_name)
+
+
+class TestN1kvHTTPResponse(test_plugin.TestV2HTTPResponse,
+                           N1kvPluginTestCase):
+    pass
+
+
+class TestN1kvPorts(test_plugin.TestPortsV2,
+                    N1kvPluginTestCase):
+    pass
+
+
+class TestN1kvNetworks(test_plugin.TestNetworksV2,
+                       N1kvPluginTestCase):
+    pass
