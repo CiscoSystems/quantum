34a494b8acbbef2cd9cfd859741092f6937250bd
Abhishek Raut <abhishek@os-controller-abhishek.(none)>
2013-01-29 12:04:03 -0800

    Addressing code review comments. (Section 3.15, 3.18, 3.19, 3.21, 3.23 addressed)

--- quantum/plugins/cisco/n1kv/n1kv_client.py
+++ quantum/plugins/cisco/n1kv/n1kv_client.py
@@ -74,7 +74,7 @@ class Client(n1kv_profile_db.Profile_db_mixin):
         """
         Fetches a list of all profiles
         """
-        return self.get(self.profiles_path, params=_params)
+        return self._get(self.profiles_path, params=_params)
 
     def create_bridge_domain(self, network, **_params):
         """
@@ -83,7 +83,7 @@ class Client(n1kv_profile_db.Profile_db_mixin):
         body = {'name': network['name'] + '_bd',
                 'segmentId': network[provider.SEGMENTATION_ID],
                 'groupIp': network[n1kv_profile.MULTICAST_IP], }
-        return self.post(self.bridge_domains_path, body=body, params=_params)
+        return self._post(self.bridge_domains_path, body=body, params=_params)
 
     def create_vmnd(self, network, **_params):
         """
@@ -98,19 +98,19 @@ class Client(n1kv_profile_db.Profile_db_mixin):
             body.update({'vlan': network[provider.SEGMENTATION_ID]})
         if network[provider.NETWORK_TYPE] == const.TYPE_VXLAN:
             body.update({'bridgeDomain': network['name'] + '_bd'})
-        return self.post(self.vmnds_path, body=body, params=_params)
+        return self._post(self.vmnds_path, body=body, params=_params)
 
     def update_vmnd(self, vmnd, body):
         """
         Updates a VMND on the VSM
         """
-        return self.post(self.vmnd_path % (vmnd), body=body)
+        return self._post(self.vmnd_path % (vmnd), body=body)
 
     def delete_vmnd(self, vmnd, **_params):
         """
         Deletes a VMND on the VSM
         """
-        return self.delete(self.vmnd_path % (vmnd))
+        return self._delete(self.vmnd_path % (vmnd))
 
     def create_fnd(self, profile, **_params):
         """
@@ -120,19 +120,19 @@ class Client(n1kv_profile_db.Profile_db_mixin):
         body = {'name': profile['name'],
                 'id': profile['profile_id'],
                 'fabricNetworkName': 'test'}
-        return self.post(self.fnds_path, body=body, params=_params)
+        return self._post(self.fnds_path, body=body, params=_params)
 
     def update_fnd(self, fnd, body):
         """
         Updates a FND on the VSM
         """
-        return self.post(self.fnd_path % (fnd), body=body)
+        return self._post(self.fnd_path % (fnd), body=body)
 
     def delete_fnd(self, fnd, **_params):
         """
         Deletes a FND on the VSM
         """
-        return self.delete(self.fnd_path % (fnd))
+        return self._delete(self.fnd_path % (fnd))
 
     def create_ip_pool(self, subnet, **_params):
         """
@@ -155,7 +155,7 @@ class Client(n1kv_profile_db.Profile_db_mixin):
                 'ipAddressSubnet': netmask,
                 'name': subnet['name'],
                 'gateway': subnet['gateway_ip'], }
-        return self.post(self.ip_pools_path, body=body, params=_params)
+        return self._post(self.ip_pools_path, body=body, params=_params)
 
     def create_port(self, port, name, **_params):
         """
@@ -170,39 +170,33 @@ class Client(n1kv_profile_db.Profile_db_mixin):
                 'portProfileId': port[n1kv_profile.PROFILE_ID],
                 'portId': port['id'],
                 'macAddress': port['mac_address']}
-        return self.post(self.ports_path, body=body, params=_params)
+        return self._post(self.ports_path, body=body, params=_params)
 
     def update_port(self, port, body):
         """
         Updates a Port on the VSM
         """
-        return self.post(self.port_path % (port), body=body)
+        return self._post(self.port_path % (port), body=body)
 
     def delete_port(self, port, **_params):
         """
         Deletes a Port on the VSM
         """
-        return self.delete(self.port_path % (port))
+        return self._delete(self.port_path % (port))
 
     def __init__(self, **kwargs):
         """ Initialize a new client for the Plugin v2.0. """
         self.format = 'json'
-        self.retries = 0
-        self.retry_interval = 1
         self.action_prefix = '/api/hyper-v'
-        self.hosts = self.get_vsm_hosts(TENANT)
+        self.hosts = self._get_vsm_hosts(TENANT)
 
-    def _handle_fault_response(self, status_code, response_body):
-        # Create exception with HTTP status code and message
-        pass
-
-    def do_request(self, method, action, body=None, headers=None, params=None):
+    def _do_request(self, method, action, body=None, headers=None, params=None):
         """
         Perform the HTTP request
         """
         action = self.action_prefix + action
         if headers is None  and  self.hosts:
-            headers = self.get_header(self.hosts[0])
+            headers = self._get_header(self.hosts[0])
         if body:
             body = self.serialize(body)
             # body = json.dumps(body)
@@ -212,7 +206,7 @@ class Client(n1kv_profile_db.Profile_db_mixin):
         conn.request(method, action, body, headers)
         resp = conn.getresponse()
         replybody = resp.read()
-        status_code = self.get_status_code(resp)
+        status_code = self._get_status_code(resp)
         LOG.debug("abs status_code %s\n", status_code)
         if status_code in (httplib.OK, httplib.ACCEPTED, httplib.NO_CONTENT):
             return self.deserialize(replybody, status_code)
@@ -221,7 +215,7 @@ class Client(n1kv_profile_db.Profile_db_mixin):
         else:
             self._handle_fault_response(status_code, replybody)
 
-    def get_status_code(self, response):
+    def _get_status_code(self, response):
         """
         Returns the integer status code from the response, which
         can be either a Webob.Response (used in testing) or httplib.Response
@@ -239,7 +233,7 @@ class Client(n1kv_profile_db.Profile_db_mixin):
         if data is None:
             return None
         elif type(data) is dict:
-            return Serializer().serialize(data, self.content_type())
+            return Serializer().serialize(data, self._content_type())
         else:
             raise Exception("unable to serialize object of type = '%s'" %
                             type(data))
@@ -252,12 +246,12 @@ class Client(n1kv_profile_db.Profile_db_mixin):
             return data
         try:
             return Serializer(self._serialization_metadata).deserialize(
-                              data, self.content_type('xml'))
+                              data, self._content_type('xml'))
         except:
             if status_code == 200:
                 LOG.debug("Created VMND/FND\n")
 
-    def content_type(self, format=None):
+    def _content_type(self, format=None):
         """
         Returns the mime-type for either 'xml' or 'json'.  Defaults to the
         currently set format
@@ -266,45 +260,23 @@ class Client(n1kv_profile_db.Profile_db_mixin):
             format = self.format
         return "application/%s" % (format)
 
-    def retry_request(self, method, action, body=None,
-                      headers=None, params=None):
-        """
-        Call do_request with the default retry configuration. Only
-            idempotent requests should retry failed connection attempts.
-
-            :raises: ConnectionFailed if the maximum # of retries is exceeded
-
-
-        max_attempts = self.retries + 1
-        for i in xrange(max_attempts):
-            try:
-                return self.do_request(method, action, body=body,
-                           headers=headers, params=params)
-            except:
-            if i < self.retries:
-            _logger.debug(_('Retrying connection to quantum service'))
-            time.sleep(self.retry_interval)
-        """
-        pass
-
-    def delete(self, action, body=None, headers=None, params=None):
-        return self.do_request("DELETE", action, body=body,
+    def _delete(self, action, body=None, headers=None, params=None):
+        return self._do_request("DELETE", action, body=body,
                                headers=headers, params=params)
 
-    def get(self, action, body=None, headers=None, params=None):
-        return self.do_request("GET", action, body=body,
+    def _get(self, action, body=None, headers=None, params=None):
+        return self._do_request("GET", action, body=body,
                                   headers=headers, params=params)
 
-    def post(self, action, body=None, headers=None, params=None):
-        # Do not retry POST requests to avoid the orphan objects problem.
-        return self.do_request("POST", action, body=body,
+    def _post(self, action, body=None, headers=None, params=None):
+        return self._do_request("POST", action, body=body,
                                headers=headers, params=params)
 
-    def put(self, action, body=None, headers=None, params=None):
-        return self.retry_request("PUT", action, body=body,
+    def _put(self, action, body=None, headers=None, params=None):
+        return self._do_request("PUT", action, body=body,
                                   headers=headers, params=params)
 
-    def get_vsm_hosts(self, tenant_id):
+    def _get_vsm_hosts(self, tenant_id):
         """
         Returns a list of VSM ip addresses.
         """
@@ -314,7 +286,7 @@ class Client(n1kv_profile_db.Profile_db_mixin):
             host_list.append(cr[const.CREDENTIAL_NAME])
         return host_list
 
-    def get_header(self, host_ip):
+    def _get_header(self, host_ip):
         """
         Returns a header with auth info for the VSM
         """
