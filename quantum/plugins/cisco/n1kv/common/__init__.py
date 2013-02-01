536d9dd9abe659371eb7fc11a268f09855ea7186
Rudrajit Tapadar <rtapadar@cisco.com>
2013-01-31 16:37:15 -0800

    Changing copyright information in init file

--- quantum/plugins/cisco/n1kv/common/__init__.py
+++ quantum/plugins/cisco/n1kv/common/__init__.py
@@ -13,8 +13,3 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-#
-# @author Rudrajit Tapadar, Cisco Systems, Inc.
-"""
-Init module for N1kv 
-"""
