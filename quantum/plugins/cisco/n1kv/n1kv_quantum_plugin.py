c03038c96654696ae47f44e3a55d8dad3a8127a9
Abhishek Raut <abhishek@n1k-grizzly.(none)>
2013-06-10 08:14:38 -0700

    Clean up create port and get rid of --meta option to pass a port-id during nova boot. Instead use --nic port-id parameter.

--- quantum/plugins/cisco/n1kv/n1kv_quantum_plugin.py
+++ quantum/plugins/cisco/n1kv/n1kv_quantum_plugin.py
@@ -740,14 +740,18 @@ class N1kvQuantumPluginV2(db_base_plugin_v2.QuantumDbPluginV2,
         """
         self._add_dummy_profile_for_test(port)
 
+        if 'device_id' in port['port'] and port['port']['device_owner'] in \
+                ['network:dhcp', 'network:router_interface']:
+            p_profile_name = conf.CISCO_N1K.default_policy_profile
+            p_profile = self._get_policy_profile_by_name(p_profile_name)
+            port['port']['n1kv:profile_id'] = p_profile['id']
+
         profile_id_set = False
         if n1kv_profile.PROFILE_ID in port['port']:
             profile_id = port['port'].get(n1kv_profile.PROFILE_ID)
             profile_id_set = attributes.is_attr_set(profile_id)
 
         if profile_id_set:
-            # If it is a dhcp port, profile id is
-            # populated with network profile id.
             profile_id = self._process_policy_profile(context,
                                                       port['port'])
             LOG.debug('create port: profile_id=%s', profile_id)
@@ -764,42 +768,6 @@ class N1kvQuantumPluginV2(db_base_plugin_v2.QuantumDbPluginV2,
                 self.delete_port(context, pt['id'])
             else:
                 LOG.debug("Created port: %s", pt)
-                return pt
-        elif 'device_id' in port['port'].keys():
-            if port['port']['device_owner'] == 'network:dhcp' or \
-                    port['port']['device_owner'] == 'network:router_interface':
-                # Grab profile id from the network
-                LOG.debug("create dhcp port")
-                p_profile_name = conf.CISCO_N1K.default_policy_profile
-                p_profile = self._get_policy_profile_by_name(p_profile_name)
-                port['port']['n1kv:profile_id'] = p_profile['id']
-                tenant_id = port['port']['tenant_id']
-                instance_id = port['port']['device_id']
-                device_owner = port['port']['device_owner']
-                # Create this port
-                cport = self.create_port(context, port)
-                LOG.debug("DHCP PORT UUID: %s\n", port)
-                pt = self.get_port(context, cport['id'])
-                pt['device_owner'] = device_owner
-                if 'fixed_ip' in port:
-                    pt['fixed_ips'] = cport['fixed_ip']
-                pt['device_id'] = instance_id
-                port['port'] = pt
-                pt = self.update_port(context, pt['id'], port)
-                return pt
-            else:
-                tenant_id = port['port']['tenant_id']
-                instance_id = port['port']['device_id']
-                device_owner = port['port']['device_owner']
-                port_id = self._get_instance_port_id(tenant_id, instance_id)
-                LOG.debug("PORT UUID: %s\n", port_id)
-                pt = self.get_port(context, port_id['port_id'])
-                pt['device_owner'] = device_owner
-                if 'fixed_ip' in port:
-                    pt['fixed_ips'] = port['port']['fixed_ip']
-                pt['device_id'] = instance_id
-                port['port'] = pt
-                pt = self.update_port(context, pt['id'], port)
                 return pt
 
     def _add_dummy_profile_for_test(self, obj):
@@ -830,17 +798,12 @@ class N1kvQuantumPluginV2(db_base_plugin_v2.QuantumDbPluginV2,
 
     def update_port(self, context, id, port):
         """ Update port parameters """
+        LOG.debug("Update port: %s", id)
         if self.agent_vsm:
             original_port = super(N1kvQuantumPluginV2, self).get_port(context,
                                                                       id)
         port = super(N1kvQuantumPluginV2, self).update_port(context, id, port)
         self._extend_port_dict_profile(context, port)
-        if self.agent_vsm:
-            if original_port['admin_state_up'] != port['admin_state_up']:
-                vm_network = n1kv_db_v2.get_vm_network(
-                    port[n1kv_profile.PROFILE_ID],
-                    port['network_id'])
-                self._send_update_port_request(port, vm_network['name'])
         return port
 
     def delete_port(self, context, id):
