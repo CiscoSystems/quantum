32a3364ad1a543d319243b6100baddba530e05eb
Kyle Mestery <kmestery@cisco.com>
2013-06-11 07:22:02 +0000

    Add proper copyright notice to all ODL files.

--- quantum/plugins/opendaylight/agent/odl_quantum_agent.py
+++ quantum/plugins/opendaylight/agent/odl_quantum_agent.py
@@ -20,7 +20,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-# @author: Kyle Mestery
+# @author: Kyle Mestery, Cisco Systems, Inc.
 
 import httplib
 import socket
